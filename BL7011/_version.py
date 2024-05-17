78fef4f81af0514bfe189a3ff36991f3db86f6b4
Damian Günzing <65827185+gnzng@users.noreply.github.com>
2024-05-17 14:05:51 -0700

    basic framework

---
+++ BL7011/_version.py
@@ -0,0 +1,6 @@
+#! python
+
+__name__ = "BL7011"
+__date__ = "2024-05-17"
+__version__ = "0.1.0"
+__authors__ = "Damian Guenzing|dguenzing@lbl.gov\n\tDayne Y. Sasaki|DYSasaki@lbl.gov\n\tRyan Tumbleson|rtumbleson@lbl.gov"

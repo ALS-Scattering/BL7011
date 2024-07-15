0b81c662bf77881062f7aaaa1c59bfb0479f005c
Damian Günzing <65827185+gnzng@users.noreply.github.com>
2024-07-15 15:41:10 -0700

    added test data for 'import_broken_h5' function

--- BL7011/test_import_functions.py
+++ BL7011/test_import_functions.py
@@ -3,13 +3,13 @@ from BL7011.import_functions import import_broken_h5
 
 print(
     import_broken_h5(
-        "BL7011/test_data/test_missing_frames.h5",
+        "BL7011/test_data/missing_frames16x16.h5",
         average=10,
         verbose=True,
         roi=[0, 10, 0, 10],
         missing_frames=[0, 1],
         eps=0.01,
-        for_roi=True,
-        save_to_h5=True
+        for_roi=False,
+        save_to_h5=False
     ).shape
 )

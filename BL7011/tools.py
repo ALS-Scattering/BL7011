fd8994ae896ba20032ad98682e2794197a3b9430
Damian Günzing <65827185+gnzng@users.noreply.github.com>
2024-07-18 16:14:48 -0700

    improved h5tree and get_positions_from_bluesky_json, reorganized and added test files, generalized tests

--- BL7011/tools.py
+++ BL7011/tools.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import math
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+import warnings as w
 
 
-def get_positions_from_bluesky_json(jsonfilename: str, motornames: list) -> dict:
+def get_positions_from_bluesky_json(jsonfilename: str, motornames: list = []) -> dict:
     """
     When in the bluesky exporter None is selected it exports the collected
     detector data in an .h5 file while the recorded metadata from labview
@@ -18,14 +19,14 @@ def get_positions_from_bluesky_json(jsonfilename: str, motornames: list) -> dict
     Parameters
     ----------
     jsonfilename : str
-        whole path of the .json-file exported by bluesky exporter
+        Whole path of the .json-file exported by bluesky exporter.
     motornames : list
-        names of the motor mentioned in the .json as list
+        Names of the motor mentioned in the .json as list.
 
     Returns
     -------
     positions : dict
-        motor positions of the requested motornames
+        Motor positions of the requested motornames.
     """
     # Opening JSON file, Error handling will be done automatically when file not found
     f = open(jsonfilename)
@@ -42,6 +43,29 @@ def get_positions_from_bluesky_json(jsonfilename: str, motornames: list) -> dict
     # create empty dict to catch the motor positions
     all_positions = {}
 
+    # if no motornames are given, take all motors
+    for n in range(len(data)):
+        try:
+            available_motornames = list(data[n][1]["data"].keys())
+            break
+        except:
+            pass
+
+    # get motors which are not in the list
+    motors_not_in_list = [
+        item for item in motornames if item not in available_motornames
+    ]
+
+    if motors_not_in_list or not motornames:
+        motornames = available_motornames
+        if motors_not_in_list:
+            w.warn(
+                f"The following motors are not in the list: {motors_not_in_list}. "
+                f"Available motors are: {available_motornames}. "
+                "Taking all available motors.",
+                UserWarning,
+            )
+
     for motorname in motornames:
         position_list = []
         if isinstance(data, list):
@@ -54,17 +78,6 @@ def get_positions_from_bluesky_json(jsonfilename: str, motornames: list) -> dict
         else:
             raise SyntaxError("unknown format of .json file " + jsonfilename)
 
-        # if no motor position was found, run through the file again and note down the available motors
-        if len(position_list) == 0:
-            for n in range(len(data)):
-                try:
-                    available_motors = list(data[n][1]["data"].keys())
-                    break
-                except:
-                    pass
-            raise Warning(
-                f"No motor positions found for {motorname}. Choose from the following: {available_motors}"
-            )
         # attach motor name and positions to the dict
         all_positions[motorname] = position_list
     # return motor positions as a numpy array
@@ -153,37 +166,98 @@ def where_is_my_frame_missing(
     return outliers
 
 
-def h5tree(h5filename: str) -> None:
+def h5tree(h5filename: str, return_paths: bool = False) -> None:
     """
-    Simple to print out the structure of a H5 file and the shape of the stored datasets.
+    Prints the structure of an HDF5 file and the shape of the stored datasets.
+    Optionally returns all possible paths as a list of strings.
 
     Parameters
     ----------
     h5filename : str
-        Whole path of the .h5-file.
-
+        Full path of the .h5 file.
+    return_paths : bool, optional
+        Whether to return the paths of all datasets and groups (default is False).
 
     Returns
     -------
-    None , but prints the structure of the h5 file.
+    None or list of str
+        Prints the structure of the HDF5 file. If return_paths is True, returns a list of paths.
     """
 
-    def recursive_print(val, pre=""):
-        items = len(val)
+    paths = []
+
+    def recursive_print(val, pre="", path=""):
+        """
+        Recursively prints the structure of an h5py.Group, showing hierarchy with ASCII art.
+        Optionally collects all paths.
+
+        Parameters
+        ----------
+        val : h5py.Group
+            The h5py.Group object to be printed.
+        pre : str
+            The prefix used to format the output to show hierarchy.
+        path : str
+            The current path in the HDF5 file.
+        """
+        items = len(val)  # Number of items in the current group
         for key, item in val.items():
-            is_last_item = items == 1
-            items -= 1
+            is_last_item = items == 1  # Check if this is the last item in the group
+            items -= 1  # Decrement the item count
+
+            current_path = f"{path}/{key}" if path else key
+            if return_paths:
+                paths.append(current_path)
 
             if isinstance(item, h5py.Group):
+                # Print the group name with appropriate formatting
                 print(pre + ("└── " if is_last_item else "├── ") + key)
-                recursive_print(item, pre + ("    " if is_last_item else "│   "))
+                # Recursively print the contents of the group
+                recursive_print(
+                    item, pre + ("    " if is_last_item else "│   "), current_path
+                )
             else:
+                # Determine the shape of the dataset, or mark it as "scalar" if it doesn't have a length
                 item_shape = item.shape if hasattr(item, "__len__") else "scalar"
-                # print(item_shape)
+                # Print the dataset name with its shape and appropriate formatting
                 print(
                     pre + ("└── " if is_last_item else "├── ") + f"{key} ({item_shape})"
                 )
 
+    # Open the HDF5 file in read mode
     with h5py.File(h5filename, "r") as hf:
-        print(hf)
-        recursive_print(hf)
+        print(f"\nfilename: {h5filename}")  # Print the filename
+        print(hf)  # Print the HDF5 file object
+        recursive_print(hf)  # Print the structure of the HDF5 file
+
+    if return_paths:
+
+        def filter_final_paths(paths):
+            """
+            Filters out non-final paths from a list of HDF5 paths.
+
+            Parameters
+            ----------
+            paths : list of str
+                List of HDF5 paths.
+
+            Returns
+            -------
+            list of str
+                List of final paths.
+            """
+            # Sort the paths to ensure that parent paths come before child paths
+            paths.sort()
+
+            final_paths = []
+            for i, path in enumerate(paths):
+                # Check if the current path is the final path by comparing with the next path in the sorted list
+                if i + 1 == len(paths) or not paths[i + 1].startswith(path + "/"):
+                    final_paths.append(path)
+
+            return final_paths
+
+        paths = filter_final_paths(paths)
+        paths = sorted(paths)
+
+        return paths
